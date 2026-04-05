4b9cfaf08da1287822fdfd277b0c8669f1803af5
Abhiram <abhiramvallem2007@gmail.com>
2026-04-05 20:06:01 +0530

    Changed structures/polynomial code

--- structures/polynomial.c
+++ structures/polynomial.c
@@ -10,139 +10,134 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a Polynomial Term
-typedef struct _node 
+#define MAX_TERMS 50
+
+// Structure for an individual term
+typedef struct
 {
-    int coeff;
+    float coeff;
     int exp;
-    struct _node *next;
-} node;
+} Term;
 
-// Function to create a new node (Similar to polyAppend)
-node* createNode(int c, int e) 
+// Nested structure for the Polynomial ADT
+typedef struct
 {
-    if (c == 0) return NULL; // Ignore terms with zero coefficient
-    node *newNode = (node*)malloc(sizeof(node));
-    newNode->coeff = c;
-    newNode->exp = e;
-    newNode->next = NULL;
-    return newNode;
-}
+    Term terms[MAX_TERMS];
+    int numTerms;
+} Polynomial;
 
-// Function to append a term to the end of a list
-void append(node **head, int c, int e) 
+// i) Initializing all terms of the polynomial as zeros
+void initPolynomial(Polynomial *p)
 {
-    node *newNode = createNode(c, e);
-    if (newNode == NULL) return;
-
-    if (*head == NULL) 
+    p->numTerms = 0;
+    for (int i = 0; i < MAX_TERMS; i++)
     {
-        *head = newNode;
-    } 
-    else 
-    {
-        node *temp = *head;
-        while (temp->next != NULL) 
-        {
-            temp = temp->next;
-        }
-        temp->next = newNode;
+        p->terms[i].coeff = 0.0;
+        p->terms[i].exp = 0;
     }
 }
 
-// The "Merge" Addition Logic (Following your array method)
-node* add(node *p1, node *p2) 
+// ii) Appending each term of the polynomial
+void appendTerm(Polynomial *p, float coeff, int exp)
 {
-    node *p3 = NULL;
-    node *i = p1; // Acts like your index 'i'
-    node *j = p2; // Acts like your index 'j'
-
-    while (i != NULL && j != NULL) 
+    if (p->numTerms >= MAX_TERMS)
     {
-        if (i->exp == j->exp) 
-        {
-            append(&p3, i->coeff + j->coeff, i->exp);
-            i = i->next; 
-            j = j->next;
-        } 
-        else if (i->exp > j->exp) 
-        {
-            append(&p3, i->coeff, i->exp);
-            i = i->next;
-        } 
-        else 
-        {
-            append(&p3, j->coeff, j->exp);
-            j = j->next;
-        }
+        printf("Error: Polynomial capacity reached.\n");
+        return;
     }
-
-    // Append remaining terms from p1
-    while (i != NULL) 
-    {
-        append(&p3, i->coeff, i->exp);
-        i = i->next;
-    }
-
-    // Append remaining terms from p2
-    while (j != NULL) 
-    {
-        append(&p3, j->coeff, j->exp);
-        j = j->next;
-    }
-
-    return p3;
+    // Simple implementation: adds term to the end
+    p->terms[p->numTerms].coeff = coeff;
+    p->terms[p->numTerms].exp = exp;
+    p->numTerms++;
 }
 
-void display(node *head) 
+// iv) To display the polynomial
+void displayPolynomial(Polynomial p)
 {
-    if (head == NULL) 
+    if (p.numTerms == 0)
     {
         printf("0\n");
         return;
     }
-    node *temp = head;
-    while (temp != NULL) 
+    for (int i = 0; i < p.numTerms; i++)
     {
-        printf("%dx^%d", temp->coeff, temp->exp);
-        temp = temp->next;
-        if (temp != NULL && temp->coeff > 0) printf(" + ");
-        else if (temp != NULL) printf(" ");
+        printf("%.1fx^%d", p.terms[i].coeff, p.terms[i].exp);
+        if (i < p.numTerms - 1)
+        {
+            printf(" + ");
+        }
     }
     printf("\n");
 }
 
-int main() 
+// iii) To perform addition between two polynomials
+Polynomial addPolynomials(Polynomial p1, Polynomial p2)
 {
-    node *poly1 = NULL, *poly2 = NULL, *poly3 = NULL;
-    int n, i, c, e;
+    Polynomial result;
+    initPolynomial(&result);
+    int i = 0, j = 0;
 
-    // Input for Poly 1
-    printf("Enter number of terms for Poly 1: ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++) 
+    // Standard merge-sort style addition for sorted exponents
+    while (i < p1.numTerms && j < p2.numTerms)
     {
-        printf("Term %d (coeff exp): ", i + 1);
-        scanf("%d %d", &c, &e);
-        append(&poly1, c, e);
+        if (p1.terms[i].exp > p2.terms[j].exp)
+        {
+            appendTerm(&result, p1.terms[i].coeff, p1.terms[i].exp);
+            i++;
+        } else if (p1.terms[i].exp < p2.terms[j].exp)
+        {
+            appendTerm(&result, p2.terms[j].coeff, p2.terms[j].exp);
+            j++;
+        } else {
+            float sumCoeff = p1.terms[i].coeff + p2.terms[j].coeff;
+            if (sumCoeff != 0)
+            {
+                appendTerm(&result, sumCoeff, p1.terms[i].exp);
+            }
+            i++;
+            j++;
+        }
     }
 
-    // Input for Poly 2
-    printf("Enter number of terms for Poly 2: ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++) 
+    // Append remaining terms
+    while (i < p1.numTerms)
     {
-        printf("Term %d (coeff exp): ", i + 1);
-        scanf("%d %d", &c, &e);
-        append(&poly2, c, e);
+        appendTerm(&result, p1.terms[i++].coeff, p1.terms[i].exp);
     }
+    while (j < p2.numTerms)
+    {
+        appendTerm(&result, p2.terms[j++].coeff, p2.terms[j].exp);
+    }
+    
 
-    printf("\nPoly 1: "); display(poly1);
-    printf("Poly 2: "); display(poly2);
+    return result;
+}
 
-    poly3 = add(poly1, poly2);
+int main()
+{
+    Polynomial poly1, poly2, sum;
 
-    printf("Result: "); display(poly3);
+    initPolynomial(&poly1);
+    initPolynomial(&poly2);
+
+    // Creating Poly 1: 5x^2 + 3x^1
+    appendTerm(&poly1, 5, 2);
+    appendTerm(&poly1, 3, 1);
+
+    // Creating Poly 2: 4x^2 + 2x^0
+    appendTerm(&poly2, 4, 2);
+    appendTerm(&poly2, 2, 0);
+
+    printf("Polynomial 1: ");
+    displayPolynomial(poly1);
+
+    printf("Polynomial 2: ");
+    displayPolynomial(poly2);
+
+    sum = addPolynomials(poly1, poly2);
+
+    printf("Sum: ");
+    displayPolynomial(sum);
 
     return 0;
 }
